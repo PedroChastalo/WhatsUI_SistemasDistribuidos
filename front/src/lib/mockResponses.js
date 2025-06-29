d04184bc07bb1886a92f6b91877220b10116fc9e
Pedro <pedrochastalo@gmail.com>
2025-06-29 15:56:20 -0300

    chore: reorganize project structure and documentation into docs folder

--- front/src/lib/mockResponses.js
+++
@@ -1,7 +0,0 @@
-// Este arquivo foi esvaziado para desativar o modo mock
-// Mantido apenas para compatibilidade com importações existentes
-
-// Objeto vazio para substituir os dados mockados
-const mockResponses = {};
-
-export default mockResponses;
